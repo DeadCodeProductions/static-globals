efc9856ba4bf40bc4bcbe3a73026122f028c865b
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2023-02-02 17:42:19 +0100

    Add forgotten files

--- build_python_wheel_local.sh
+++ build_python_wheel_local.sh
@@ -12,7 +12,7 @@ cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
 
 ninja 
 cd ..
-cp build/bin/make-globals-static python_src/static-globals
+cp build/bin/make-globals-static python_src/static_globals
 cp setup.py.in setup.py
 sed -i "s~THIS_DIR~$(pwd)~g" setup.py
 python -m build
